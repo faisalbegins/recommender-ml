503714a64047bd6e264fad2ddfe45c09308a06f6
Faisal Ahmed <faisalbegins@gmail.com>
2021-07-30 16:46:50 -0500

    added ml trianing script

---
+++ train.py
@@ -0,0 +1,77 @@
+import pandas as pd
+import numpy as np
+import pickle
+
+
+def prepare_data():
+    # read two data files
+    credits_df = pd.read_csv('/Users/Faisal/Development/recommender-storage/data/tmdb_credits.csv')
+    movies_df = pd.read_csv('/Users/Faisal/Development/recommender-storage/data/tmdb_movies.csv')
+
+    # Join two dataset based on id (movie_id)
+    credits_df.columns = ['id','tittle','cast','crew']
+    df = movies_df.merge(credits_df, on='id')
+
+    # drop unnecessary columns
+    df = df.drop(['budget', 'homepage',
+                  'original_title', 'production_companies',
+                  'release_date', 'revenue', 'runtime',
+                  'spoken_languages', 'status', 'tagline', 'tittle'], axis=1)
+
+    # rename columns
+    df.columns = ['genres', 'id', 'keywords',
+                  'language', 'overview',
+                  'popularity', 'countries',
+                  'title', 'vote_average',
+                  'vote_count', 'cast', 'crew']
+
+    # fill null values with empty string
+    df['overview'] = df['overview'].fillna('')
+
+    # find mean vote accross the whole dataset
+    C = df['vote_average'].mean()
+
+    # find minimum vote requires to be listed
+    m = df['vote_count'].quantile(.9)
+
+    # all all the movies that matches minimum vote requirements
+    movies_filter_by_vote_count = df.copy().loc[df['vote_count'] >= m]
+
+    # weighted rating calculation
+    def weighted_rating(x, m=m, C=C):
+        v = x['vote_count']
+        R = x['vote_average']
+        # Calculation based on the IMDB formula
+        return (v / (v + m) * R) + (m / (m + v) * C)
+
+    # Add a new feature 'score' and calculate its value with `weighted_rating()`
+    movies_filter_by_vote_count['score'] = movies_filter_by_vote_count.apply(weighted_rating, axis=1)
+
+    # find top trending movies based on score
+    trending_movies = movies_filter_by_vote_count.sort_values('score', ascending=False)
+
+    # find top movies based on popularity
+    popular_movies = df.copy().sort_values('popularity', ascending=False)
+
+    models = [
+        (trending_movies, '/Users/Faisal/Development/recommender-storage/models/trending.model'),
+        (popular_movies, '/Users/Faisal/Development/recommender-storage/models/popular.model'),
+        (df, '/Users/Faisal/Development/recommender-storage/models/generic.model')
+    ]
+
+    return models
+
+
+def serialize_models(models):
+    for model in models:
+        outfile = open(model[1], 'wb')
+        pickle.dump(model[0], outfile)
+
+
+def main():
+    models = prepare_data()
+    serialize_models(models)
+
+
+if __name__ == '__main__':
+    main()
